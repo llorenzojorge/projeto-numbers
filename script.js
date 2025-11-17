b62717ce00be27060fc0629f9503b30740a0dd05
Lorenzo Jorge <161716578+llorenzojorge@users.noreply.github.com>
2025-11-17 20:22:02 -0300

    function anti caracteres para os inputs

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
-// Seleciona os elementos do formulário
+// Seleciona os elementos do formulário.
 const form = document.querySelector("form")
 const numbers = document.getElementById("numbers")
 const min = document.getElementById("min")
 const max = document.getElementById("max")
 const toggle = document.getElementById("toggle")
 
-// Captura o evento de submit do form para obter os valores
+// Função reutilizável para remover caracteres dos inputs. 
+function removeCharacter(input) {
+    // Substitui caracteres não númericos por uma string vazia.
+    let value = input.value.replace(/\D/g, "")
+    // Garante que não retorne NaN quando o input estiver vazio.
+    input.value = value ? parseInt(value, 10) : ""
+}
+
+// Aplica função removeCharacter() para os 3 inputs principais.
+[numbers, min, max].forEach(input => {
+    input.addEventListener("input", () => removeCharacter(input))
+})
+
+// Captura o evento de submit do form para obter os valores.
 form.onsubmit = (event) => {
     event.preventDefault()
 
+    // Cria objeto com os valores dos input's.
     const newDraw = {
         numbers: numbers.value,
         min: min.value,
